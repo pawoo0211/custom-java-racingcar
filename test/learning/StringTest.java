6cfffded59670e5373064d872a64dfef05b320aa
pawoo0211 <pawoo0211@naver.com>
2024-04-06 22:52:41 +0900

    refactor: Extract deleteParentheses method

--- test/learning/StringTest.java
+++ test/learning/StringTest.java
@@ -33,15 +33,22 @@ public class StringTest {
     }
 
     class Slice {
+        private final char LEFT_PARENTHESES = '(';
+        private final char LIGHT_PARENTHESES = ')';
+
         public String[] sliceByComma(String input) {
-            if (input.charAt(0) == '(') {
+            input = deleteParentheses(input);
+            return input.split(",");
+        }
+
+        private String deleteParentheses(String input) {
+            if (input.charAt(0) ==  LEFT_PARENTHESES) {
                 input = input.substring(1);
             }
-
-            if (input.charAt(input.length()-1) == ')') {
+            if (input.charAt(input.length() - 1) == LIGHT_PARENTHESES) {
                 input = input.substring(0, input.length()-1);
             }
-            return input.split(",");
+            return input;
         }
     }
 }
