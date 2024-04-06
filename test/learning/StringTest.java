b018ca4e5930ddc8bd9f2e06184187d44c37e4c1
pawoo0211 <pawoo0211@naver.com>
2024-04-06 22:45:07 +0900

    Test: String 클래스에 대한 학습 테스트 - 요구사항 2

--- test/learning/StringTest.java
+++ test/learning/StringTest.java
@@ -7,22 +7,40 @@ import org.junit.jupiter.api.Test;
 @DisplayName("String 클래스 학습 테스트")
 public class StringTest {
     private final Slice slice = new Slice();
+
     @Test
-    void splitCommaTest() {
+    @DisplayName("문자열을 ,를 기준으로 분할한다.")
+    void splitByComma() {
         String given = "1,2";
         Assertions.assertThat(slice.sliceByComma(given))
                 .containsExactly("1","2");
     }
 
     @Test
-    void splitNonCommaTest() {
+    @DisplayName("하나의 문자만 있을 때 정상 동작이 되는지 확인한다.")
+    void splitNonComma() {
         String given = "1";
         Assertions.assertThat(slice.sliceByComma(given))
                 .containsExactly("1");
     }
 
+    @Test
+    @DisplayName("()를 제거한 후 문자열을 ,를 기준으로 분할한다.")
+    void splitInParentheses() {
+        String given = "(1,2)";
+        Assertions.assertThat(slice.sliceByComma(given))
+                .containsExactly("1","2");
+    }
+
     class Slice {
         public String[] sliceByComma(String input) {
+            if (input.charAt(0) == '(') {
+                input = input.substring(1);
+            }
+
+            if (input.charAt(input.length()-1) == ')') {
+                input = input.substring(0, input.length()-1);
+            }
             return input.split(",");
         }
     }
