2bbb3ae76adc5ecd257e367360ee757bdd53ae41
samuel_eximbay <samuel@eximbay.com>
2024-04-11 21:23:15 +0900

    feat: 문자열 구분자

---
+++ src/calculator/Delimiter.java
@@ -0,0 +1,26 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Delimiter {
+    private static String COMMA = ",";
+    private static String COLON = ":";
+    private static String COMMA_OR_COLON = COMMA + "|" + COLON;
+    private static Pattern PATTERN = Pattern.compile("//(.)\n(.*)");
+
+    public static String[] splitByCommaOrColon(String input) {
+        return input.split(COMMA_OR_COLON);
+    }
+
+    public static String[] splitByCustomDelimiter(String input) {
+        String[] tokens = {};
+        Matcher matcher = PATTERN.matcher(input);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            tokens = matcher.group(2).split(customDelimiter);
+            return tokens;
+        }
+        return tokens;
+    }
+}
